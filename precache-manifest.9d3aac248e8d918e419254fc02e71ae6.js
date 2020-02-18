6bde5184dd65adcd4ddf49ba06c23a475c7e4d93
0pippi0 <j.romany91@gmail.com>
2020-02-18 23:24:18 +0900

    Updates

---
+++ precache-manifest.9d3aac248e8d918e419254fc02e71ae6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07c2e584fbd16811e2484b5fe33ec296",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "f5a6d9da42497533e16a",
+    "url": "/todo-list/static/css/main.4ff502f2.chunk.css"
+  },
+  {
+    "revision": "8209f9a9c650819c5298",
+    "url": "/todo-list/static/js/2.25fdc318.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/todo-list/static/js/2.25fdc318.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5a6d9da42497533e16a",
+    "url": "/todo-list/static/js/main.47dd952e.chunk.js"
+  },
+  {
+    "revision": "3c8bc7aa72310e7d0f4e",
+    "url": "/todo-list/static/js/runtime-main.901ffa1a.js"
+  }
+]);
